743635b8acea24ff4476d53d2f8ed11ba512b156
autr <gilbert.sinnott@gmail.com>
2018-10-23 18:30:20 +0200

    Rm .txt file

--- generate.sh
+++ generate.sh
@@ -1,5 +1,4 @@
 cat Header.md > README.md
-# Header.md >> README.md
 URL=./
 for f in $(find . -name '*.png' -or -name '*.svg'); 
 do echo $URL${f:(+2)} &&  echo '![Icon]('$URL${f:(+2)}')\n\n' '`'${f:(2)}'`' '\n\n' >> README.md
